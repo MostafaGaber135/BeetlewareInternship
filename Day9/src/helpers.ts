302135608392393c241f81b3f5d0a2fafe8fa200
MostafaGaber135 <mostafagaber1234560@gmail.com>
2025-11-23 11:49:54 +0200

    Day9

---
+++ Day9/src/helpers.ts
@@ -0,0 +1,4 @@
+export function box<T>(value: T): T {
+    return value;
+  }
+  
---
+++ Day9/src/result.ts
@@ -0,0 +1,6 @@
+import { box } from "./helpers";
+const b1 = box(123);
+const b2 = box("hello");
+
+console.log(b1);
+console.log(b2);
